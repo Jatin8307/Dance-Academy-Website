30fc33d68c7addfbc80035fed4ebe7c3de3ff2ed
Jatin8307 <96301386+Jatin8307@users.noreply.github.com>
2023-05-20 12:08:43 +0530

    From Working started using mongoose

--- app.js
+++ app.js
@@ -4,6 +4,24 @@ const fs = require("fs");
 const app = express();
 const port = 7000;
 
+const mongoose = require('mongoose');
+const bodyparser = require("body-parser");
+mongoose.connect('mongodb://127.0.0.1:27017/ContactInfo');
+
+// define mongoose schema
+const ContactSchema = new mongoose.Schema({
+    name: String,
+    age: String,
+    gender: String,
+    phone: String,
+    email: String,
+    desc: String,
+    address: String
+  });
+
+const Contact = mongoose.model('Contact', ContactSchema);
+
+
 // For serving static files
 app.use('/static', express.static('static'))
 app.use(express.urlencoded())
@@ -24,6 +42,17 @@ app.get('/contact', (req, res)=>{
     const params = { }
     res.status(200).render('contact.pug', params);
 })
+
+app.post('/contact', (req, res)=>{
+    var mydata = new Contact(req.body);
+    mydata.save().then(()=>{
+        res.send("THIS ITEM HAS BEEN SAVE TO THE DATABASE");
+    }).catch(()=>{
+        res.status(400).send("ITEM WAS NOT SAVED TO DATABASE");
+    })
+    // res.status(200).render('contact.pug', params);
+})
+
 
 // START THE SERVER
 app.listen(port, ()=>{
